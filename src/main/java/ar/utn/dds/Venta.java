4f04d9ee40997940b8e524713781b50da5aa6fdb
Julieta Abuin <julietabuin@hotmail.com>
2018-04-05 00:12:46 -0300

    Ahora tiene la lista de prendas de la venta

--- src/main/java/ar/utn/dds/Venta.java
+++ src/main/java/ar/utn/dds/Venta.java
@@ -1,6 +1,8 @@
 package ar.utn.dds;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Venta {
+	List<Prenda> listaPrendas = new ArrayList<Prenda>();
 	Venta(MetodoDePago pago) {
 		this.formadepago = pago;
 	}
